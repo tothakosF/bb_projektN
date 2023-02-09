0c1fb6858449d82832cacfb3c3c827a6e4448ed8
Toth Akos <toth.akos.ferenc24725@gmail.com>
2023-02-09 22:45:06 +0100

    files

---
+++ projektHet/projektHet/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Automation.Peers;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace projektHet
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Tanulok> sracok = new List<Tanulok>();
+        public int hazi;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void megnyit_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog allomanyMegnyitas = new OpenFileDialog();
+            allomanyMegnyitas.Filter = "Szöveges állományok (*.txt)|*.txt|Minden állomány (*.*)|*.*";
+            if (allomanyMegnyitas.ShowDialog() == true)
+            {
+                foreach (var item in File.ReadLines(allomanyMegnyitas.FileName))
+                {
+                    Tanulok tanulo = new Tanulok(item);
+                    sracok.Add(tanulo);
+                }
+            }
+            tanulokGrid.ItemsSource = sracok;
+            tanulokGrid.AlternatingRowBackground = Brushes.Gray;
+            tanulokGrid.BorderBrush = new SolidColorBrush(Colors.Black);
+        }
+
+        private void mentes_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog allomanyMentes = new SaveFileDialog();
+            allomanyMentes.Filter = "Szöveges állományok (*.txt)|*.txt|Minden állomány (*.*)|*.*";
+            if (allomanyMentes.ShowDialog() == true)
+            {
+                StreamWriter sw = new StreamWriter(allomanyMentes.FileName);
+                foreach (var item in sracok)
+                {
+                    sw.WriteLine(item.Név + ";" + item.Első + ";" + item.Második + ";" + item.Harmadik + ";" + item.Negyedik + ";" + item.Ötödik + ";" + item.Hatodik + ";" + item.Hetedik + ";" + item.Nyolcadik);
+                }
+                sw.Close();
+            }
+        }
+
+        private void haziB_Click(object sender, RoutedEventArgs e)
+        {
+            hazi = 0;
+            foreach (var item in sracok)
+            {
+                if (item.Első == "")
+                {
+                    hazi++;
+                }
+                if (item.Második == "")
+                {
+                    hazi++;
+                }
+                if (item.Harmadik == "")
+                {
+                    hazi++;
+                }
+                if (item.Negyedik == "")
+                {
+                    hazi++;
+                }
+                if (item.Ötödik == "")
+                {
+                    hazi++;
+                }
+                if (item.Hatodik == "")
+                {
+                    hazi++;
+                }
+                if (item.Hetedik == "")
+                {
+                    hazi++;
+                }
+                if (item.Nyolcadik == "")
+                {
+                    hazi++;
+                }
+            }
+            haziL.Content = hazi.ToString();
+        }
+    }
+}
---
+++ projektHet/projektHet/Taulok.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projektHet
+{
+    internal class Tanulok
+    {
+        public string Név { get; set; }
+        public string Első { get; set; }
+        public string Második { get; set; }
+        public string Harmadik { get; set; }
+        public string Negyedik { get; set; }
+        public string Ötödik { get; set; }
+        public string Hatodik { get; set; }
+        public string Hetedik { get; set; }
+        public string Nyolcadik { get; set; }
+
+        public Tanulok(string sor)
+        {
+            string[] reszek = sor.Split(';');
+            Név = reszek[0];
+            Első = reszek[1];
+            Második = reszek[2];
+            Harmadik = reszek[3];
+            Negyedik = reszek[4];
+            Ötödik = reszek[5];
+            Hatodik = reszek[6];
+            Hetedik = reszek[7];
+            Nyolcadik = reszek[8];
+        }
+    }
+}
